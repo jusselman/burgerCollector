68ef38b8c2fc8fc2701360b6f5e6ac190bfd2998
jusselman <usselmanjosh@gmail.com>
2019-09-15 23:22:21 -0700

    add details

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Burger(models.Model):
+    name = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
+    quality = models.IntegerField()
+    
+
+
+
+# class Photo(models.Model):
+#     url = models.CharField(max_length=200)
+#     burger = models.ForeignKey(Burger, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"Photo for burger_id: {self.burger_id} @{self.url}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Burger
 
 # Imports 
 from django.http import HttpResponse
@@ -11,19 +12,26 @@ def home(request):
 def about(request): 
     return render(request, 'about.html')
 
+def burgers_detail(request, burger_id):
+  burger = Burger.objects.get(id=burger_id)
+  return render(request, 'burgers/detail.html', { 'burger': burger })
+
+# class Burger: 
+#     def __init__(self, name, origin, description, quality):
+#         self.name = name
+#         self.origin = origin
+#         self.description = description
+#         self.quality = quality
+
+
 def burgers_index(request):
-    return render(request, 'burgers/index.html', { 'cats': burgers })
+    burgers = Burger.objects.all()
+    return render(request, 'burgers/index.html', { 'burgers': burgers })
 
-class Burger: 
-    def __init__(self, name, origin, description, quality):
-        self.name = name
-        self.origin = origin
-        self.description = description
-        self.quality = quality
-
-burgers = [
-    Burger('FourByFour', 'InNOut', 'Animal styled to perfection', 5),
-    Burger('Big Mac', 'MacDonalds', 'Pleasurable for the first 5 minutes', 3),
-    Burger('Hang Over', 'Big Mouth', 'It\'s all in the name', 4 ) ,
-    Burger('Whopper', 'Burger King', 'Great when they were a dollar', 2)
-]
+ # burgers = [
+    #     Burger('FourByFour', 'InNOut', 'Animal styled to perfection', 5),
+    #     Burger('Big Mac', 'MacDonalds', 'Pleasurable for the first 5 minutes', 3),
+    #     Burger('Hang Over', 'Big Mouth', 'It\'s all in the name', 4 ), 
+    #     Burger('Whopper', 'Burger King', 'Great when they were a dollar', 2)
+    # ]
+    # print(burgers)
